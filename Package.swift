459a7e1e6439a285ec7f53cf7f3d4eee3a087f52
Tanner Nelson <tanner@bluebite.com>
2016-05-18 16:24:24 -0400

    0.3.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentMongo",
     dependencies: [
-    	.Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 0),
+    	.Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3),
     	.Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 9)
     ]
 )
