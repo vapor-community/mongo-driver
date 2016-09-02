63dd7c96b4d9b482c193c071a8ddf8dedbf21528
Paul Rolfe <paul.a.rolfe@gmail.com>
2016-09-02 13:00:18 -0400

    Update to latest fluent and mongo kitten

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentMongo",
     dependencies: [
-    	.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 4),
-    	.Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 0, minor: 9)
+    	.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 0, minor: 10),
+    	.Package(url: "https://github.com/PlanTeam/MongoKitten.git", majorVersion: 1, minor: 5)
     ]
 )
