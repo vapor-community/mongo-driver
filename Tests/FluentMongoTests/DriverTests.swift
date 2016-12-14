120dfe6f7adb4b24d9c869fb58e788acd693397f
Zsolt Váradi <vzsg@users.noreply.github.com>
2016-12-14 11:42:08 +0100

    Add missing test for the Linux test plan

--- Tests/FluentMongoTests/DriverTests.swift
+++ Tests/FluentMongoTests/DriverTests.swift
@@ -17,6 +17,7 @@ class DriverTests: XCTestCase {
         return [
             ("testConnectFailing", testConnectFailing),
             ("testSaveClearFind", testSaveClearFind),
+            ("testModify", testModify)
         ]
     }
     
