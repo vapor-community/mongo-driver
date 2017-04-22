dfa2f460b3d93289caa095b94e9bac48da452f28
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-04-22 14:21:11 +0200

    Remove unnecessary Vapor dependency

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
 let package = Package(
     name: "FluentMongo",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
     	.Package(url: "https://github.com/vapor/fluent.git", beta),
-    	.Package(url: "https://github.com/OpenKitten/MongoKitten.git", Version(0,0,22)),
+    	.Package(url: "https://github.com/OpenKitten/MongoKitten.git", Version(0,0,25)),
     ]
 )
