4f605bfbbdf4fc805fd9f6824844b01169540599
Joannis Orlandos <joannis@orlandos.nl>
2017-10-12 10:06:46 +0200

    separate swift4 package description

---
+++ Package@swift-4.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "MongoDriver",
+    products: [
+        .library(name: "MongoDriver", targets: ["MongoDriver"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "2.4.1")),
+        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .upToNextMinor(from: "4.0.16"))
+    ],
+    targets: [
+        .target(name: "MongoDriver", dependencies: ["Fluent", "MongoKitten"]),
+        .testTarget(name: "MongoDriverTests", dependencies: ["MongoDriver", "FluentTester"])
+    ]
+)
