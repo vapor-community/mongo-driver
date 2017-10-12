00eff577b04ccbb0fe57d3ad8aed625126bdfa47
Joannis Orlandos <joannis@orlandos.nl>
2017-10-12 10:04:47 +0200

    revert package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,9 @@
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "MongoDriver",
-    products: [
-        .library(name: "MongoDriver", targets: ["MongoDriver"])
-    ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from: "2.4.1")),
-        .package(url: "https://github.com/OpenKitten/MongoKitten.git", .exact("4.0.16"))
-    ],
-    targets: [
-        .target(name: "MongoDriver", dependencies: ["Fluent", "MongoKitten"]),
-        .testTarget(name: "MongoDriverTests", dependencies: ["MongoDriver", "FluentTester"])
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
+        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 4, minor: 0),
     ]
 )
